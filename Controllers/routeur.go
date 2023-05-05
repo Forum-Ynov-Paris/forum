1d9500c59f34bc90a9f63e5ee721453eb1884701
BuildTools <unconfigured@null.spigotmc.org>
2023-05-05 16:29:14 +0200

    batiste tu fais chier

--- Controllers/routeur.go
+++ Controllers/routeur.go
@@ -10,23 +10,16 @@ import (
 func Routeur(db DB.DBController, store *sessions.CookieStore) {
 	http.HandleFunc("/login", Client.Login)
 	http.HandleFunc("/register", Client.Register)
-<<<<<<< HEAD
-<<<<<<< HEAD
 	http.HandleFunc("/post", Client.Post)
 	Client.LoginPost(db, store)
 	Client.HomeClient(db, store)
 	Client.RegisterPost(db)
 	Client.PostClient(db, store)
-=======
-	http.HandleFunc("/createpost", Client.createPost)
-=======
+	http.HandleFunc("/createpost", Client.CreatePost)
 	Client.Search(db, store)
->>>>>>> e68f5fddf0543cf4ac7534f9a36f3180e280699a
 	Client.LoginPost(db, store)
 	Client.HomeClient(db, store)
-	Client.CreatePost(store)
+	Client.HandleCreatepost(db, store)
 	Client.RegisterPost(db)
 	Client.HandleCreatepost(db, store)
-
->>>>>>> 7aa84e417f315ae5f54899f84c44050c826e1d4c
 }
